62f6c655ec556d5dff56d97365fffdbe0e827d45
Suttawee Promin <kooku190642@gmail.com>
2020-09-23 01:14:13 +0700

    Plearnhub

---
+++ js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener('prechange', function(event) {
+    document.querySelector('ons-toolbar .center')
+     
+  });
+  document.addEventListener('init', function(event) {
+    var page = event.target;
+  
+    if (page.id === 'homemovie') {
+      page.querySelector('#Movie1').onclick = function () {
+        document.querySelector('#myNavigator').pushPage('views/detail1.html')
+    }
+    page.querySelector('#Movie2').onclick = function () {
+        document.querySelector('#myNavigator').pushPage('views/detail2.html')
+    }
+    page.querySelector('#Movie3').onclick = function () {
+        document.querySelector('#myNavigator').pushPage('views/detail3.html')
+    }
+    } else if (page.id === 'page2') {
+      page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+    }
+  });
